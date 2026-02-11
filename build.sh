227a16a3da5cf8e8d8569fca519f4c7cae605828
abd alrahman kanawati <kanawatiabdalrahman@gmail.com>
2026-02-11 04:36:02 +0300

    Update build commands in vercel.json and add build.sh for Flutter installation

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+# Install Flutter
+echo "Installing Flutter..."
+git clone https://github.com/flutter/flutter.git -b stable --depth 1 $HOME/flutter
+export PATH="$PATH:$HOME/flutter/bin"
+
+# Verify Flutter installation
+flutter --version
+
+# Get dependencies
+flutter pub get
+
+# Build web app
+flutter build web --release -O4 --pwa-strategy offline-first
